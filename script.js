2aefcb1a148925a7a281d03e98d2bd919ed10b40
Eric Treadway <eric.s.treadway@gmail.com>
2021-03-16 20:10:37 -0500

    fetches food name, pic and instructions

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Eric
-
+// food
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.meals[0])
+        document.getElementById('nameOfFood').innerHTML = data.meals[0].strMeal
+        document.getElementById('picOfFood').src = data.meals[0].strMealThumb
+        document.getElementById('instructionsOfFood').innerHTML = data.meals[0].strInstructions
+    })
 
 
 
