23ac1450788d51c67a27a65b589003eb34590fbb
csolis1 <celida.solis@outlook.com>
2021-03-16 21:39:47 -0400

    fetch img, name and instructions

--- script.js
+++ script.js
@@ -9,11 +9,12 @@
 
 // Celida
 
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
 
-
-
-
-
-
-
-
+     console.log(data.drinks[0])
+     document.getElementById("nameOfDrink").innerHTML = data.drinks[0].strDrink
+     document.getElementById("directions").innerHTML = data.drinks[0].strInstructions
+     document.getElementById("picOfDrink").src = data.drinks[0].strDrinkThumb
+    });
