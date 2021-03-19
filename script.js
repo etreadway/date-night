6cbdf162f44170ac9fc6e1af8d8fb8fee15c6708
Eric Treadway <eric.s.treadway@gmail.com>
2021-03-19 17:42:22 -0500

    added section for desserts

--- script.js
+++ script.js
@@ -7,43 +7,12 @@ resetButtonObj.addEventListener("click", getRandomPairing)
 
 
 function getRandomPairing() {
-    //clearing list items
-
     //food
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.meals[0])
-        // food name picture and instructions 
-        document.getElementById('nameOfFood').innerHTML = data.meals[0].strMeal
-        document.getElementById('picOfFood').src = data.meals[0].strMealThumb + '/preview'
-        document.getElementById('instructionsOfFood').innerHTML = data.meals[0].strInstructions
-        
-
-        // ingredient list
-        var foodIngredientList = document.getElementById('ingredientsOfFood')
-       
-        foodIngredientList.innerHTML = null
-
-        for (var i=1; i<=20; i++) {
-            //checks to make sure it is not an empty item
-            if (data.meals[0]['strIngredient' + i] != null 
-            && data.meals[0]['strIngredient' + i] != ""){
-                let newListItem = document.createElement("li")
-                newListItem.innerHTML = data.meals[0]['strIngredient' + i]
-                newListItem.innerHTML += ' - ' + data.meals[0]['strMeasure' + i]
-                foodIngredientList.append(newListItem)
-            }
-
-            
-        }
-    })
-
-//var resetButton = document.createElement("button");
-//document.body.append(resetButton)
-
-//drink
-fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    getRandomMeal()
+    //dessert
+    getRandomDessert()
+    //drink
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     .then(res => res.json())
     .then(data => {
             // drink name picture and instructions
@@ -60,20 +29,77 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
             for( var i=1; i<=15; i++) {
                 
                 if(data.drinks[0]['strIngredient'+ i] != null
-                    && data.drinks[0]['strIngredient'+ i] != "") {
-                
-                let newListItem = document.createElement('li')
-                newListItem.innerHTML = data.drinks[0]['strIngredient' + i]
-                newListItem.innerHTML += ' - ' + data.drinks[0]['strMeasure' + i]
-                drinkIngredients.append(newListItem)
-            
-                 }
+                && data.drinks[0]['strIngredient'+ i] != "") {
+                    let newListItem = document.createElement('li')
+                    newListItem.innerHTML = data.drinks[0]['strIngredient' + i]
+                    newListItem.innerHTML += ' - ' + data.drinks[0]['strMeasure' + i]
+                    drinkIngredients.append(newListItem)
+                }
             }
-
-
-        });
+    });
 
 
 }
 
 
+function getRandomMeal(){
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        // makes sure meal is not dessert
+        if (data.meals[0].strCategory == 'Dessert'){
+            getRandomMeal()
+        }else{
+            console.log(data.meals[0])
+            // food name picture and instructions 
+            document.getElementById('nameOfFood').innerHTML = data.meals[0].strMeal
+            document.getElementById('picOfFood').src = data.meals[0].strMealThumb + '/preview'
+            document.getElementById('instructionsOfFood').innerHTML = data.meals[0].strInstructions
+            // ingredient list
+            var foodIngredientList = document.getElementById('ingredientsOfFood')
+            foodIngredientList.innerHTML = null
+            for (var i=1; i<=20; i++) {
+                // checks to make sure it is not an empty item
+                if (data.meals[0]['strIngredient' + i] != null 
+                && data.meals[0]['strIngredient' + i] != ""){
+                    let newListItem = document.createElement("li")
+                    newListItem.innerHTML = data.meals[0]['strIngredient' + i]
+                    newListItem.innerHTML += ' - ' + data.meals[0]['strMeasure' + i]
+                    foodIngredientList.append(newListItem)
+                }
+            }
+        }
+    })
+}
+
+function getRandomDessert(){
+    // fetch random dessert id
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert')
+    .then(res => res.json())
+    .then(data => {
+        var idMeal = data.meals[Math.floor(Math.random() * data.meals.length)].idMeal
+        // fetch data on random dessert
+        fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + idMeal)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.meals[0])
+
+            document.getElementById('nameOfDessert').innerHTML = data.meals[0].strMeal
+            document.getElementById('picOfDessert').src = data.meals[0].strMealThumb + '/preview'
+            document.getElementById('instructionsOfDessert').innerHTML = data.meals[0].strInstructions
+            // ingredient list
+            var foodIngredientList = document.getElementById('ingredientsOfDessert')
+            foodIngredientList.innerHTML = null
+            for (var i=1; i<=20; i++) {
+                //checks to make sure it is not an empty item
+                if (data.meals[0]['strIngredient' + i] != null 
+                && data.meals[0]['strIngredient' + i] != ""){
+                    let newListItem = document.createElement("li")
+                    newListItem.innerHTML = data.meals[0]['strIngredient' + i]
+                    newListItem.innerHTML += ' - ' + data.meals[0]['strMeasure' + i]
+                    foodIngredientList.append(newListItem)
+                }
+            }
+        })
+    })
+}
